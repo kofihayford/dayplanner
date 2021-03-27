09fdf7d430817c3dd69743e976d0da49c9e1c19f
kofithebass <kofi@kofihayford.com>
2021-03-27 13:51:43 -0400

    added pseudocode to the script.

---
+++ Develop/script.js
@@ -0,0 +1,14 @@
+// Create/Declare the appropriate variables and grab the appropriate elements from the DOM 
+const saveBtn = $(".saveBtn")
+
+
+// Create functions that update the appropriate elements of the DOM with the user's inputs. Check the listener and activating functions with console logs to begin with 
+
+
+
+// Activate Local Storage 
+
+
+
+
+// Call the functions you created
