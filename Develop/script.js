1e1897f8be6f0550d8f6a572b9e701b0f9caf1ee
kofithebass <kofi@kofihayford.com>
2021-03-28 23:25:37 -0400

    connected Luxon to the HTML
    Added basic pseudocode to the script file.
    Minor changes to the CSS

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,7 @@
 const saveBtn = $(".saveBtn")
 
 
+
 // Create functions that update the appropriate elements of the DOM with the user's inputs. Check the listener and activating functions with console logs to begin with 
 
 
